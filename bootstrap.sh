227fea90ef6fcdf315c649465bbf40e0474df9ce
Jon Bardin <diclophis@gmail.com>
2017-04-05 00:23:08 -0700

    remove shell

--- bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-psql --host=$PG_HOST --username=$PG_USERNAME --list | grep fluentd || createdb --host=$PG_HOST --username=$PG_USERNAME fluentd
-echo "CREATE TABLE IF NOT EXISTS fluentd (tag Text, time Timestamptz, record Jsonb);" | psql --host=$PG_HOST --username=$PG_USERNAME
